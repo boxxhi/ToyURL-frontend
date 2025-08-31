dfcc371fa1a69fede8ba6942f80abdc92e7bed2e
XmoraxX <moraxtao610@gmail.com>
2025-08-31 17:51:41 -0600

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,6 @@ export default {
 </script>
 
 <template>
+  <HeaderComponent />
   <MainComponent />
 </template>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,10 +5,53 @@ export default {
 </script>
 
 <template>
-  <header>
+  <header class="header">
+    <div class="logo">
+      <img src="C:\Users\user\Desktop\probando vue\origin\ToyURL-frontend\src\images\Picture2.png" 
+      alt="Logo" />
+    </div>
+    <div class="actions">
+      <button class="login-btn">Iniciar Sesion</button>
+      <button class="register-btn">Registrarme</button>
+    </div>
   </header>
 </template>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #12314f;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.logo img {
+  height: 40px;
+}
+
+.actions {
+  display: flex;
+  gap: 10px;
+}
+
+.login-btn {
+  background: transparent;
+  border: 1px solid #1976d2;
+  color: #1976d2;
+  padding: 8px 12px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.register-btn {
+  background: #28a745;
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 
 </style>
